a73b67daf7649254732bf54aba230f7a970da1ae
Unknown <tereshin.tesha@gmail.com>
2018-11-20 16:29:08 +0000

    all vectors are changed to vector pointers

--- test_2D/include/DrawField.h
+++ test_2D/include/DrawField.h
@@ -13,11 +13,11 @@ public:
     DrawField() { }
     ~DrawField() { }
 
-    cv::Mat convert_field_to_image( std::vector<uchar> *uchar_field, std::vector<double> field, int res_x, int res_y );
+    cv::Mat convert_field_to_image(std::vector<uchar> *uchar_field, const std::vector<double> *field, const int res_x, const int res_y );
 
-    void draw_field         ( std::vector<uchar> uchar_field, std::vector<double> field, int res_x, int res_y, std::string file_name );
-    void draw_grey_isolines ( std::vector<uchar> uchar_field, std::vector<double> field, int res_x, int res_y, std::string file_name );
-    void draw_rgb_isolines  ( std::vector<uchar> uchar_field, std::vector<double> field, int res_x, int res_y, std::string file_name );
+    void draw_field         ( std::vector<uchar> *uchar_field, const std::vector<double> *field, const int res_x, const int res_y, std::string file_name="" );
+    void draw_grey_isolines ( std::vector<uchar> *uchar_field, const std::vector<double> *field, const int res_x, const int res_y, std::string file_name="" );
+    void draw_rgb_isolines  ( std::vector<uchar> *uchar_field, const std::vector<double> *field, const int res_x, const int res_y, std::string file_name="" );
 };
 
 } // namespace draw
