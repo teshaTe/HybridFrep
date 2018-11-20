af572ae11d5e004eecc414409b18786e92cc6a9a
Unknown <tereshin.tesha@gmail.com>
2018-11-20 16:28:37 +0000

    the constructor is changed to a more simple variant, other stuff should be called manually. Also changed functions working with fields, range is fixed.

--- test_2D/include/DDT.h
+++ test_2D/include/DDT.h
@@ -23,13 +23,15 @@ public:
     DistanceField(cv::Mat img, int b_sh);
     ~DistanceField(){ }
 
-    inline std::vector<double> get_DDT()            { return DDT;  }
-    inline std::vector<double> get_smoothed_SDF() { return SDDT; }
-    inline std::vector<double> get_sparse_interp_SDF() { return ISDDT; }
-    inline double get_SDDT_values(cv::Vec2f uv) { return SDDT[ static_cast<int>((res_x-b_sh) * uv[0]) +
-                                                               static_cast<int>((res_y-b_sh) * uv[1]) * res_x ]; }
+    inline std::vector<double> get_DDT()               { return DDT; }
 
-    std::vector<double> zoom_in_field (std::vector<double> field, cv::Point2i start_p, cv::Vec2i reg_s, cv::Vec2i fin_res );
+    std::vector<double> zoom_in_field (const std::vector<double> *field, const cv::Point2i start_p, const cv::Vec2i reg_s, const cv::Vec2i fin_res , const int resX);
+    std::vector<double> finalize_field(const std::vector<double> *field, int resX, int resY);
+
+    std::vector<double> smooth_field(const std::vector<double> *DDT, int resX, int resY);
+    std::vector<double> generate_interpolated_field(const std::vector<double> *DT, int resX, int resY,
+                                                    int shiftX, int shiftY, int interpolation_type = 0 );
+    std::vector<double> generate_extrapolated_field( const std::vector<double> DDT, int interpolation_type = 0 );
 
     //functions for generating distance transform using binarized input image
 private:
@@ -42,13 +44,6 @@ private:
     void create_grid(cv::Mat *img);
     void compare_grid_points(std::vector<Point> &grid, Point &point, int offsetx, int offsety, int x, int y);
     void merge_grids(std::vector<double> &grid);
-
-    //functions for generating in-between values if field is zoomed in/out
-private:
-    std::vector<double> smooth_field(const std::vector<double> DDT, int resX, int resY);
-    std::vector<double> generate_interpolated_field(const std::vector<double> DDT, int resX, int resY,
-                                                    int shiftX, int shiftY, int interpolation_type = 0 );
-    std::vector<double> generate_extrapolated_field( const std::vector<double> DDT, int interpolation_type = 0 );
 
 private:
     int res_x, res_y, b_sh;
