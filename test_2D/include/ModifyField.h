b93dcb4cbaf175a74d20c8f3feb5fc80ee2127e0
Unknown <tereshin.tesha@gmail.com>
2018-12-14 12:22:02 +0000

    fixed bicubic interpolation and implement it in 2 variants

--- test_2D/include/ModifyField.h
+++ test_2D/include/ModifyField.h
@@ -24,16 +24,20 @@ public:
     std::vector<double> smooth_field ( const std::vector<double> *DDT,   int resX, int resY);
     std::vector<double> zoom_field   ( const std::vector<double> *field, const cv::Point2i start_p,
                                        const cv::Vec2i reg_s, const cv::Vec2i fin_res);
-    std::vector<double> interpolate_field( const std::vector<double> *field, const cv::Vec2i cur_res, const cv::Vec2i fin_res);
-    double get_bilinear_interpolated_val (const std::vector<double> *DT, cv::Vec2i res, cv::Vec2f indXY );
+    std::vector<double> interpolate_field( const std::vector<double> *field, const cv::Vec2i cur_res,
+                                          const cv::Vec2i fin_res, interpolation inter);
+    double get_bilinear_interpolated_val ( const std::vector<double> *DT, cv::Vec2i res, cv::Vec2f indXY );
+    double get_bicubic_interpolated_val2  ( const std::vector<double> *DT, cv::Vec2i res, cv::Vec2f indXY );
+    double get_bicubic_interpolated_val  ( const std::vector<double> *DT, cv::Vec2i res, cv::Vec2f indXY );
+
 
     std::vector<double> diff_fields(const std::vector<double> *field1, const std::vector<double> *field2 , double multi);
 
 private:
     //std::vector<double> generate_b_spline_interpolated_field   ( const std::vector<double> *DT, int resX, int resY, int shiftX, int shiftY );
-    std::vector<double> generate_bicubic_interpolated_field    ( const std::vector<double> *DT, int resX, int resY, int shiftX, int shiftY );
-    std::vector<double> generate_bilinearly_interpolated_field ( const std::vector<double> *DT, int resX, int resY, int shiftX, int shiftY );
     std::vector<double> generate_extrapolated_field( const std::vector<double> DDT, int interpolation_type = 0 );
+
+    double get_cubic_interpolated_val(const std::vector<double> *DT, cv::Vec2i res, cv::Vec2f indXY , double var, int shY);
 
     inline int clipWithBounds( int n, int n_min, int n_max ) { return n > n_max ? n_max : ( n < n_min ? n_min : n );}
 
--- test_2D/src/ModifyField.cpp
+++ test_2D/src/ModifyField.cpp
@@ -29,7 +29,7 @@ std::vector<double> ModifyField::smooth_field( const std::vector<double> *DT, in
     return inter_result;
 }
 
-/*std::vector<double> ModifyField::generate_b_spline_interpolated_field(const std::vector<double> *DT, int resX, int resY, int shiftX, int shiftY)
+/*double ModifyField::get_bicubic_interpolated_val(const std::vector<double> *DT, int resX, int resY, int shiftX, int shiftY)
 {
 
 
@@ -37,92 +37,100 @@ std::vector<double> ModifyField::smooth_field( const std::vector<double> *DT, in
 
     return ;
 }*/
-
-std::vector<double> ModifyField::generate_bicubic_interpolated_field(const std::vector<double> *DT, int resX, int resY, int shiftX, int shiftY)
+double ModifyField::get_cubic_interpolated_val( const std::vector<double> *DT, cv::Vec2i res, cv::Vec2f indXY, double var, int shY )
 {
-    std::vector<double> fin_field;
-    fin_field.resize( (resX - shiftX) * (resY - shiftY) );
+    int x = clipWithBounds( int(indXY[0]), 0, res[0]-4 );
+    int y = clipWithBounds( int(indXY[1]), 0, res[1]-4 );
+    return DT->at(x+1+(y+shY)*res[0]) + 0.5 * var*( DT->at(x+2+(y+shY)*res[0]) - DT->at(x+(y+shY)*res[0]) +
+           var*(2.0*DT->at(x+(y+shY)*res[0]) - 5.0*DT->at(x+1+(y+shY)*res[0]) + 4.0*DT->at(x+2+(y+shY)*res[0]) -
+           DT->at(x+3+(y+shY)*res[0]) + var*(3.0*(DT->at(x+1+(y+shY)*res[0]) - DT->at(x+2+(y+shY)*res[0])) +
+           DT->at(x+3+(y+shY)*res[0]) - DT->at(x+(y+shY)*res[0]))));
+}
 
-    for( int y = 0; y < resY-2*shiftY; y+=shiftY )
-    {
-        for( int x = 0; x < resX-2*shiftX; x+=shiftX )
-        {
-            double f00 = DT->at(x+y*resX);
-            double f10 = DT->at((x+shiftX)+y*resX);
-            double f01 = DT->at(x+(y+shiftY)*resX);
-            double f11 = DT->at((x+shiftX)+(y+shiftY)*resX);
+double ModifyField::get_bicubic_interpolated_val2(const std::vector<double> *DT, cv::Vec2i res, cv::Vec2f indXY)
+{
+    std::vector<double> a; a.resize( 4 );
+    int x0 = clipWithBounds( int(indXY[0]), 0, res[0]-3 );
+    int y0 = clipWithBounds( int(indXY[1]), 0, res[1]-3 );
+    double x = x0 / res[0];
+    double y = y0 / res[1];
 
-            double fx00 = ( DT->at((x+shiftX)+y*resX)            - DT->at(x+y*resX) ) / shiftX;
-            double fx10 = ( DT->at((x+2*shiftX)+y*resX)          - DT->at((x+shiftX)+y*resX) ) / shiftX;
-            double fx01 = ( DT->at((x+shiftX)+(y+shiftY)*resX)   - DT->at(x+(y+shiftY)*resX) ) / shiftX;
-            double fx11 = ( DT->at((x+2*shiftX)+(y+shiftY)*resX) - DT->at((x+shiftX)+(y+shiftY)*resX) ) / shiftX;
+    a[0] = get_cubic_interpolated_val(DT, res, indXY, y, 0);
+    a[1] = get_cubic_interpolated_val(DT, res, indXY, y, 1);
+    a[2] = get_cubic_interpolated_val(DT, res, indXY, y, 2);
+    a[3] = get_cubic_interpolated_val(DT, res, indXY, y, 3);
+    double result = get_cubic_interpolated_val(&a, cv::Vec2i(4, 4), indXY, x, 0);
 
-            double fy00 = ( DT->at(x+(y+shiftY)*resX)            - DT->at(x+y*resX) ) / shiftY;
-            double fy10 = ( DT->at((x+shiftX)+(y+shiftY)*resX)   - DT->at((x+shiftX)+y*resX) ) / shiftY;
-            double fy01 = ( DT->at(x+(y+2*shiftY)*resX)          - DT->at(x+(y+shiftY)*resX) ) / shiftY;
-            double fy11 = ( DT->at((x+shiftX)+(y+2*shiftY)*resX) - DT->at((x+shiftX)+(y+shiftY)*resX) ) / shiftY;
+    return result;
+}
 
-            double fxy00_1 = ( DT->at((x+shiftX)+(y+shiftY)*resX)  - DT->at((x+shiftX)+y*resX) ) / shiftY;
-            double fxy00_2 = ( DT->at(x+(y+shiftY)*resX)           - DT->at(x+y*resX)) / shiftY;
-            double fxy00   = ( 1.0/shiftX ) * ( fxy00_1 - fxy00_2 );
 
-            double fxy10_1 = ( DT->at((x+2*shiftX)+(y+shiftY)*resX) - DT->at((x+2*shiftX)+y*resX) ) / shiftY;
-            double fxy10_2 = ( DT->at((x+shiftX)+(y+shiftY)*resX)   - DT->at((x+shiftX)+y*resX) ) / shiftY;
-            double fxy10   = ( 1.0/shiftX ) * ( fxy10_1 - fxy10_2 );
+double ModifyField::get_bicubic_interpolated_val(const std::vector<double> *DT, cv::Vec2i res, cv::Vec2f indXY)
+{
+    int x = clipWithBounds( int(indXY[0]), 0, res[0]-3 );
+    int y = clipWithBounds( int(indXY[1]), 0, res[1]-3 );
 
-            double fxy01_1 = ( DT->at((x+shiftX)+(y+2*shiftY)*resX) - DT->at((x+shiftX)+(y+shiftY)*resX) ) / shiftY;
-            double fxy01_2 = ( DT->at(x+(y+2*shiftY)*resX)          - DT->at(x+(y+shiftY)*resX) ) / shiftY;
-            double fxy01   = ( 1.0/shiftX ) * ( fxy01_1 - fxy01_2 );
+    double f00 = DT->at(x+y*res[0]);
+    double f10 = DT->at((x+1)+y*res[0]);
+    double f01 = DT->at(x+(y+1)*res[0]);
+    double f11 = DT->at((x+1)+(y+1)*res[0]);
 
-            double fxy11_1 = ( DT->at((x+2*shiftX)+(y+2*shiftY)*resX) - DT->at((x+2*shiftX)+(y+shiftY)*resX) ) / shiftY;
-            double fxy11_2 = ( DT->at((x+shiftX)+(y+2*shiftY)*resX)   - DT->at((x+shiftX)+(y+shiftY)*resX) ) / shiftY;
-            double fxy11   = ( 1.0/shiftX ) * ( fxy11_1 - fxy11_2 );
+    double fx00 = ( DT->at((x+1)+y*res[0])     - DT->at(x+y*res[0]) );
+    double fx10 = ( DT->at((x+2)+y*res[0])     - DT->at((x+1)+y*res[0]) );
+    double fx01 = ( DT->at((x+1)+(y+1)*res[0]) - DT->at(x+(y+1)*res[0]) );
+    double fx11 = ( DT->at((x+2)+(y+1)*res[0]) - DT->at((x+1)+(y+1)*res[0]) );
 
-            double a00 =  f00;
-            double a10 =  fx00;
-            double a01 =  fy00;
-            double a11 =  fxy00;
-            double a20 = -3.0*f00   + 3.0*f10   - 2.0*fx00  - fx10;
-            double a02 = -3.0*f00   + 3.0*f01   - 2.0*fy00  - fy01;
-            double a21 = -2.0*fxy00 - fxy10     - 3.0*fy00  + 3.0*fy10;
-            double a12 = -3.0*fx00  + 3.0*fx01  - 2.0*fxy00 - fxy01;
-            double a22 =  9.0*f00   - 9.0*f01   - 9.0*f10   + 9.0*f11 + 6.0*fx00 - 6.0*fx01 + 3.0*fx10 - 3.0*fx11 +
-                          4.0*fxy00 + 2.0*fxy01 + 2.0*fxy10 + fxy11   + 6.0*fy00 + 3.0*fy01 - 6.0*fy10 - 3.0*fy11;
-            double a30 =  2.0*f00   - 2.0*f10   + fx00      + fx10;
-            double a03 =  2.0*f00   - 2.0*f01   + fy00      + fy01;
-            double a31 =  fxy00     + fxy10     + 2.0*fy00  - 2.0*fy10;
-            double a13 =  2.0*fx00  - 2.0*fx01  + fxy00     + fxy01;
-            double a23 = -6.0*f00   + 6.0*f01   + 6.0*f10   - 6.0*f11 - 4.0*fx00 + 4.0*fx01 - 2.0*fx10 + 2.0*fx11 -
-                          2.0*fxy00 - 2.0*fxy01 - fxy10     - fxy11   - 3.0*fy00 - 3.0*fy01 + 3.0*fy10 + 3.0*fy11;
-            double a32 = -6.0*f00   + 6.0*f01   + 6.0*f10   - 6.0*f11 - 3.0*fx00 + 3.0*fx01 - 3.0*fx10 + 3.0*fx11 -
-                          2.0*fxy00 - fxy01     - 2.0*fxy10 - fxy11   - 4.0*fy00 - 2.0*fy01 + 4.0*fy10 + 2.0*fy11;
-            double a33 =  4.0*f00   - 4.0*f01   - 4.0*f10   + 4.0*f11 + 2.0*fx00 - 2.0*fx01 + 2.0*fx10 - 2.0*fx11 +
-                          fxy00     + fxy01     + fxy10     + fxy11   + 2.0*fy00 + 2.0*fy01 - 2.0*fy10 - 2.0*fy11;
+    double fy00 = ( DT->at(x+(y+1)*res[0])     - DT->at(x+y*res[0]) );
+    double fy10 = ( DT->at((x+1)+(y+1)*res[0]) - DT->at((x+1)+y*res[0]) );
+    double fy01 = ( DT->at(x+(y+2)*res[0])     - DT->at(x+(y+1)*res[0]) );
+    double fy11 = ( DT->at((x+1)+(y+2)*res[0]) - DT->at((x+1)+(y+1)*res[0]) );
 
-            for( int y_in = 0; y_in <= shiftY; y_in++ )
-            {
-                for( int x_in = 0; x_in <= shiftX; x_in++ )
-                {
-                    if( DT->at(x+x_in+(y+y_in)*resX) == -10000.0f )
-                    {
+    double fxy00_1 = ( DT->at((x+1)+(y+1)*res[0]) - DT->at((x+1)+y*res[0]) );
+    double fxy00_2 = ( DT->at(x+(y+1)*res[0])     - DT->at(x+y*res[0]));
+    double fxy00   = ( fxy00_1 - fxy00_2 );
 
-                        double result = a00 + y_in*a01 + y_in*y_in*a02 + std::pow(y_in, 3.0)*a03 + x_in*a10 + x_in*y_in*a11 +
-                                        x_in*y_in*y_in*a12 + x_in*std::pow(y_in, 3.0)*a13 + x_in*x_in*a20 + x_in*x_in*y_in*a21 +
-                                        std::pow(x_in*y_in, 2.0)*a22 + y_in*std::pow(x_in*y_in, 2.0)*a23 + std::pow(x_in, 3.0)*a30 +
-                                        std::pow(x_in, 3.0)*y_in*a31 + x_in*std::pow(x_in*y_in, 2.0)*a32 + std::pow(x_in*y_in, 3.0)*a33 ;
+    double fxy10_1 = ( DT->at((x+2)+(y+1)*res[0]) - DT->at((x+2)+y*res[0]) );
+    double fxy10_2 = ( DT->at((x+1)+(y+1)*res[0]) - DT->at((x+1)+y*res[0]) );
+    double fxy10   = ( fxy10_1 - fxy10_2 );
 
-                        fin_field[x+x_in + (y+y_in)*(resX-shiftX)] = std::abs(result);
-                        if(result < 0)
-                            int stop = 0;
-                    }
-                    else
-                        fin_field[x+x_in + (y+y_in)*(resX-shiftX)] = DT->at(x+x_in+(y+y_in)*resX);
-                }
-            }
-        }
-    }
+    double fxy01_1 = ( DT->at((x+1)+(y+2)*res[0]) - DT->at((x+1)+(y+1)*res[0]) );
+    double fxy01_2 = ( DT->at(x+(y+2)*res[0])     - DT->at(x+(y+1)*res[0]) );
+    double fxy01   = ( fxy01_1 - fxy01_2 );
 
-    return fin_field;
+    double fxy11_1 = ( DT->at((x+2)+(y+2)*res[0]) - DT->at((x+2)+(y+1)*res[0]) );
+    double fxy11_2 = ( DT->at((x+1)+(y+2)*res[0]) - DT->at((x+1)+(y+1)*res[0]) );
+    double fxy11   = ( fxy11_1 - fxy11_2 );
+
+    double a00 =  f00;
+    double a10 =  fx00;
+    double a01 =  fy00;
+    double a11 =  fxy00;
+    double a20 = -3.0*f00   + 3.0*f10   - 2.0*fx00  - fx10;
+    double a02 = -3.0*f00   + 3.0*f01   - 2.0*fy00  - fy01;
+    double a21 = -2.0*fxy00 - fxy10     - 3.0*fy00  + 3.0*fy10;
+    double a12 = -3.0*fx00  + 3.0*fx01  - 2.0*fxy00 - fxy01;
+    double a22 =  9.0*f00   - 9.0*f01   - 9.0*f10   + 9.0*f11 + 6.0*fx00 - 6.0*fx01 + 3.0*fx10 - 3.0*fx11 +
+                  4.0*fxy00 + 2.0*fxy01 + 2.0*fxy10 + fxy11   + 6.0*fy00 + 3.0*fy01 - 6.0*fy10 - 3.0*fy11;
+    double a30 =  2.0*f00   - 2.0*f10   + fx00      + fx10;
+    double a03 =  2.0*f00   - 2.0*f01   + fy00      + fy01;
+    double a31 =  fxy00     + fxy10     + 2.0*fy00  - 2.0*fy10;
+    double a13 =  2.0*fx00  - 2.0*fx01  + fxy00     + fxy01;
+    double a23 = -6.0*f00   + 6.0*f01   + 6.0*f10   - 6.0*f11 - 4.0*fx00 + 4.0*fx01 - 2.0*fx10 + 2.0*fx11 -
+                  2.0*fxy00 - 2.0*fxy01 - fxy10     - fxy11   - 3.0*fy00 - 3.0*fy01 + 3.0*fy10 + 3.0*fy11;
+    double a32 = -6.0*f00   + 6.0*f01   + 6.0*f10   - 6.0*f11 - 3.0*fx00 + 3.0*fx01 - 3.0*fx10 + 3.0*fx11 -
+                  2.0*fxy00 - fxy01     - 2.0*fxy10 - fxy11   - 4.0*fy00 - 2.0*fy01 + 4.0*fy10 + 2.0*fy11;
+    double a33 =  4.0*f00   - 4.0*f01   - 4.0*f10   + 4.0*f11 + 2.0*fx00 - 2.0*fx01 + 2.0*fx10 - 2.0*fx11 +
+                  fxy00     + fxy01     + fxy10     + fxy11   + 2.0*fy00 + 2.0*fy01 - 2.0*fy10 - 2.0*fy11;
+
+    double xN = x / res[0];
+    double yN = y / res[1];
+
+    double result = a00 + yN*a01 + yN*yN*a02 + std::pow(yN, 3.0)*a03 + xN*a10 + xN*yN*a11 +
+                    xN*yN*yN*a12 + xN*std::pow(yN, 3.0)*a13 + xN*xN*a20 + xN*xN*yN*a21 +
+                    std::pow(xN*yN, 2.0)*a22 + yN*std::pow(xN*yN, 2.0)*a23 + std::pow(xN, 3.0)*a30 +
+                    std::pow(xN, 3.0)*yN*a31 + xN*std::pow(xN*yN, 2.0)*a32 + std::pow(xN*yN, 3.0)*a33 ;
+
+    return result;
 }
 
 
@@ -188,7 +196,8 @@ std::vector<double> ModifyField::zoom_field( const std::vector<double> *field, c
     return  result;
 }
 
-std::vector<double> ModifyField::interpolate_field(const std::vector<double> *field, const cv::Vec2i cur_res, const cv::Vec2i fin_res)
+std::vector<double> ModifyField::interpolate_field(const std::vector<double> *field, const cv::Vec2i cur_res,
+                                                   const cv::Vec2i fin_res, interpolation inter )
 {
     std::vector<double> result;
     if( (fin_res[0])%cur_res[0] == 0 && (fin_res[1])%cur_res[1] == 0 )
@@ -208,7 +217,16 @@ std::vector<double> ModifyField::interpolate_field(const std::vector<double> *fi
         {
             cv::Vec2f indXY = { float(x)/pix_xShift, float(y)/pix_yShift };
             try {
-                result.push_back( get_bilinear_interpolated_val( field, cur_res, indXY ) );
+                if( inter == interpolation::BILINEAR )
+                    result.push_back( get_bilinear_interpolated_val( field, cur_res, indXY ) );
+                else if( inter == interpolation::BICUBIC )
+                    result.push_back( get_bicubic_interpolated_val( field, cur_res, indXY) );
+                else
+                {
+                    std::cerr << "Error: <interpolation inter> Unknown option passsed! " << std::endl;
+                    result.clear();
+                    return result;
+                }
             } catch (const std::out_of_range& e) {
                 std::cerr << "error: " << e.what() << ",   index: " << x << ", " << y << "   " <<
                              " indXY: " << indXY[0] << ", " << indXY[1] << std::endl;
