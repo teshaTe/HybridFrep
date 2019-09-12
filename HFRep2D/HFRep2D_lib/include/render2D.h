aec55a1005cf6405410a9d34bdcf47a07941ac7a
TeSha <tereshin.tesha@gmail.com>
2019-09-12 17:15:05 +0100

    updated visualizer of the signed fields, added visible isolines

--- HFRep2D/HFRep2D_lib/include/render2D.h
+++ HFRep2D/HFRep2D_lib/include/render2D.h
@@ -18,8 +18,12 @@ public:
                              int res_x, int res_y, std::string file_name="" );
     void drawGrey_isolines ( std::vector<uchar> *uchar_field, const std::vector<float> *field,
                             int res_x, int res_y, std::string file_name="" );
-    void drawRGB_isolines  ( std::vector<uchar> *uchar_field, const std::vector<float> *field,
-                            int res_x, int res_y, float thres, std::string file_name="" , bool sign = false);
+    void drawRGB_isolines  (const std::vector<float> *field, int res_x, int res_y, float thres, std::string file_name="", float thres_iso = 0.0195f);
+
+    inline cv::Mat getResultingImg() { return res; }
+
+private:
+    cv::Mat res;
 };
 
 } // namespace draw
--- HFRep2D/HFRep2D_lib/src/render2D.cpp
+++ HFRep2D/HFRep2D_lib/src/render2D.cpp
@@ -1,16 +1,22 @@
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+
 #include "include/render2D.h"
+
 #include "opencv2/highgui.hpp"
+
+//#include <SOIL/SOIL.h>
 
 #include <algorithm>
 
 namespace hfrep2D {
 
-cv::Mat render2D::convert_field_to_image( std::vector<uchar> *uchar_field, const std::vector<float> *field,
+cv::Mat render2D::convert_field_to_image( std::vector<uchar> *ufield, const std::vector<float> *field,
                                           const int res_x, const int res_y )
 {
-    uchar_field->clear();
+    ufield->clear();
     cv::Mat dst = cv::Mat( res_x, res_y, CV_8UC1 );
-    uchar_field->resize(res_x*res_y);
+    ufield->resize(res_x*res_y);
 
     float dist;
 
@@ -22,29 +28,30 @@ cv::Mat render2D::convert_field_to_image( std::vector<uchar> *uchar_field, const
             dist = std::abs( field->at(i) ) * 20.0f + 128.0f;
 
         if( dist < 0.0f )
-            uchar_field->at(i) = 0;
+            ufield->at(i) = 0;
         else if( dist > 255.0f )
-            uchar_field->at(i) = 255;
+            ufield->at(i) = 255;
         else
-            uchar_field->at(i) = static_cast<uchar>(dist);
+            ufield->at(i) = static_cast<uchar>(dist);
     }
 
-    memcpy(dst.data, uchar_field->data(), uchar_field->size()*sizeof (uchar));
+    memcpy(dst.data, ufield->data(), ufield->size()*sizeof (uchar));
     return dst;
 }
 
-void render2D::drawField( std::vector<uchar> *uchar_field, const std::vector<float> *field,
+void render2D::drawField( std::vector<uchar> *ufield, const std::vector<float> *field,
                           const int res_x, const int res_y, std::string file_name )
 {
-    cv::Mat dst = convert_field_to_image( uchar_field, field, res_x, res_y);
+    cv::Mat dst = convert_field_to_image( ufield, field, res_x, res_y);
     std::string ddt_file_name = "field_im_" + file_name + ".png";
+
     cv::imwrite(ddt_file_name, dst);
 }
 
-void render2D::drawGrey_isolines( std::vector<uchar> *uchar_field, const std::vector<float> *field,
+void render2D::drawGrey_isolines( std::vector<uchar> *ufield, const std::vector<float> *field,
                                  const int res_x, const int res_y, std::string file_name )
 {
-    convert_field_to_image( uchar_field, field, res_x, res_y );
+    cv::Mat dst = convert_field_to_image( ufield, field, res_x, res_y );
     std::vector<uchar> ISO_UDDT;
     ISO_UDDT.resize(field->size());
     int col_st = 5;
@@ -53,104 +60,91 @@ void render2D::drawGrey_isolines( std::vector<uchar> *uchar_field, const std::ve
     {
         for(int j = 0; j <= 255-col_st; j+=col_st)
         {
-            if( j <= uchar_field->at(i) && uchar_field->at(i) <= j+col_st )
+            if( j <= ufield->at(i) && ufield->at(i) <= j+col_st )
             {
                 ISO_UDDT[i] = j+col_st;
                 break;
             }
-
         }
     }
 
-    cv::Mat dst(res_x, res_y, CV_8UC1);
+    dst = cv::Mat( res_x, res_y, CV_8UC1 );
     memcpy(dst.data, ISO_UDDT.data(), ISO_UDDT.size()*sizeof (uchar));
 
     std::string ddt_file_name = "isoline_field_grey_" + file_name + ".png";
     cv::imwrite(ddt_file_name, dst);
 }
 
-void render2D::drawRGB_isolines( std::vector<uchar> *uchar_field, const std::vector<float> *field,
-                                const int res_x, const int res_y, float thres, std::string file_name, bool sign )
+void render2D::drawRGB_isolines( const std::vector<float> *field, const int res_x, const int res_y,
+                                 float thres, std::string file_name, float thres_iso )
 {
     cv::Mat dst = cv::Mat( res_x, res_y, CV_8UC3 );
+
+    std::vector<uchar> dstImg, ufield; dstImg.resize(res_x * res_y * 3);
+
+    //unsigned char *buff =  static_cast<unsigned char*>(dstImg.data());
+
     unsigned char *dst_buf = static_cast<unsigned char*>(dst.data);
     int d_ch      = dst.channels();
     size_t d_step = dst.step;
     int col_st    = 5;
 
-    convert_field_to_image(uchar_field, field, res_x, res_y);
+    convert_field_to_image(&ufield, field, res_x, res_y);
 
-    for(int y = 0; y < dst.rows; y++)
+    int col_ind = 0;
+
+    for(int y = 0; y < res_y; y++)
     {
-        for(int x = 0; x < dst.cols; x++)
+        for(int x = 0; x < res_x; x++)
         {
-            if( sign )
+            for(int i = 0; i <= 255-col_st; i+=col_st)
             {
-                for(int i = 0; i <= 255-col_st; i+=col_st)
+                if( field->at(x+y*res_x) < 0.0f )
                 {
-                    if( field->at(x+y*res_x) < 0.0f )
+                    float val = field->at(x+y*res_x);
+                    float indicator = std::fmod(val, static_cast<float>(1.0/col_st));
+
+                    if( indicator < 0.0f && indicator > -thres_iso )
                     {
-                        if( i <= uchar_field->at(x+y*res_x) && uchar_field->at(x+y*res_x) <= i+col_st )
-                        {
-                            dst_buf[x*d_ch+y*d_step]   = i+col_st;
-                            dst_buf[x*d_ch+1+y*d_step] = i+col_st;
-                            dst_buf[x*d_ch+2+y*d_step] = 0;
-                            break;
-                        }
+                        dst_buf[x*d_ch+y*d_step]   = 100;
+                        dst_buf[x*d_ch+1+y*d_step] = 100;
+                        dst_buf[x*d_ch+2+y*d_step] = 100;
+                        break;
                     }
-                    else if( field->at(x+y*res_x) < thres && field->at(x+y*res_x) > 0.0f )
+
+                    if( i <= ufield[x+y*res_x] && ufield[x+y*res_x] <= i+col_st )
                     {
-                        dst_buf[x*d_ch+y*d_step]   = 0;
-                        dst_buf[x*d_ch+1+y*d_step] = 0;
+                        dst_buf[x*d_ch+y*d_step]   = i+col_st;
+                        dst_buf[x*d_ch+1+y*d_step] = i+col_st;
                         dst_buf[x*d_ch+2+y*d_step] = 0;
                         break;
                     }
-                    else if( field->at(x+y*res_x) > 0.0 )
-                    {
-                        if( i <= uchar_field->at(x+y*res_x) && uchar_field->at(x+y*res_x) <= i+col_st )
-                        {
-                            dst_buf[x*d_ch+y*d_step]   = 0;
-                            dst_buf[x*d_ch+1+y*d_step] = i+col_st;
-                            dst_buf[x*d_ch+2+y*d_step] = i+col_st;
-                            break;
-                        }
-                    }
                 }
-            }
-            else
-            {
-                for(int i = 0; i <= 255-col_st; i+=col_st)
+                else if( field->at(x+y*res_x) < thres && field->at(x+y*res_x) >= 0.0f )
                 {
-                    if( i <= uchar_field->at(x+y*res_x) && uchar_field->at(x+y*res_x) <= i+col_st )
+                    dst_buf[x*d_ch+y*d_step]   = 0;
+                    dst_buf[x*d_ch+1+y*d_step] = 0;
+                    dst_buf[x*d_ch+2+y*d_step] = 0;
+                    break;
+                }
+                else
+                {
+                    float val = field->at(x+y*res_x);
+                    float indicator = std::fmod(val, static_cast<float>(1.0/col_st));
+
+                    if( indicator > 0.0f && indicator < thres_iso )
                     {
-                        if( i%2 == 0 )
-                        {
-                            dst_buf[x*d_ch+y*d_step]   = 9;
-                            dst_buf[x*d_ch+1+y*d_step] = i+col_st;
-                            dst_buf[x*d_ch+2+y*d_step] = 0;
-                            break;
-                        }
-                        else if( i%3 == 0)
-                        {
-                            dst_buf[x*d_ch+y*d_step]   = 0;
-                            dst_buf[x*d_ch+1+y*d_step] = 214;
-                            dst_buf[x*d_ch+2+y*d_step] = i+col_st;
-                            break;
-                        }
-                        else
-                        {
-                            dst_buf[x*d_ch+y*d_step]   = 0;
-                            dst_buf[x*d_ch+1+y*d_step] = 0;
-                            dst_buf[x*d_ch+2+y*d_step] = i+col_st;
-                            break;
-                        }
+                        dst_buf[x*d_ch+y*d_step]   = 200;
+                        dst_buf[x*d_ch+1+y*d_step] = 200;
+                        dst_buf[x*d_ch+2+y*d_step] = 200;
+                        break;
                     }
 
-                    if( field->at(x+y*res_x) < thres && field->at(x+y*res_x) > 0.0f )
+                    if( i <= ufield[x+y*res_x] && ufield[x+y*res_x] <= i+col_st )
                     {
                         dst_buf[x*d_ch+y*d_step]   = 0;
-                        dst_buf[x*d_ch+1+y*d_step] = 0;
-                        dst_buf[x*d_ch+2+y*d_step] = 0;
+                        dst_buf[x*d_ch+1+y*d_step] = i+col_st;
+                        dst_buf[x*d_ch+2+y*d_step] = i+col_st;
                         break;
                     }
                 }
@@ -158,8 +152,11 @@ void render2D::drawRGB_isolines( std::vector<uchar> *uchar_field, const std::vec
         }
     }
 
-    std::string field_file_name = "isoline_field_" + file_name + ".jpeg";
+    std::string field_file_name = "isoline_field_" + file_name + ".jpg";
     cv::imwrite(field_file_name, dst);
+    dst.copyTo(res);
+
+    //stbi_write_jpg(field_file_name.c_str(), res_x, res_y, 3, buff, -3*res_x );
 }
 
 
